2289d4212570eb5e86d314953038b575956022df
Rowdro <141366370+Rowdro@users.noreply.github.com>
2026-09-17 00:14:31 +0600

    Delete eventora-cinematic-hero directory

--- eventora-cinematic-hero/api/sslcommerz/cancel.js
+++
@@ -1,6 +0,0 @@
-import { sendBackToApp } from './_redirect.js';
-
-// SSLCommerz redirects here when the buyer cancels on the gateway page.
-export default function handler(req, res) {
-    sendBackToApp(req, res, 'cancelled');
-}
